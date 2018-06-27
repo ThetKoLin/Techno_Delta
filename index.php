8b3880e64948fa1f3b30140df6684d4dbf00014b
thetkolin <thetkolin2981834@gmail.com>
2018-06-27 23:36:08 +0630

    This is home page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Welcome Page</h1>
+	<form action="companylist.php" method="get">
+		<input type="submit" value="Companies list"><br><br>
+</form>
+<form action="add.php" method="post" >
+	<input type="submit" value="Add"><br><br>
+</form>
+</body>
+</html>
