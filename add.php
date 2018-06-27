5da2ec16cd3a40384b72eb01e670052b8410a6b4
thetkolin <thetkolin2981834@gmail.com>
2018-06-27 23:29:25 +0630

    This is home page

---
+++ add.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>New Category</h1>
+<form action="newdata.php" method="post">
+<label for="name">Company Name</label>
+<input type="text" name="name" id="name"><br>
+<label for="type">Company Type</label>
+<input type="text" name="type" id="type"><br>
+<lable for="address">Address</lable>
+<input type="text" name="address" id="address"><br>
+<lable for="ph">Phone no</lable>
+<input type="text" name="ph" id="ph">
+<br><br>
+<input type="submit" value="Add Data">
+</form>
+
+</body>
+</html>
