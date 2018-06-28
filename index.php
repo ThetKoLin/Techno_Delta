fc8524c3ec1e7e6d9dfbe847ca100c48f0c4ebb9
thetkolin <thetkolin2981834@gmail.com>
2018-06-28 08:41:12 +0630

    I change my home page

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
 		<input type="submit" value="Companies list"><br><br>
 </form>
 <form action="add.php" method="post" >
-	<input type="submit" value="Add"><br><br>
+	<input type="submit" value="Add Company"><br><br>
 </form>
+<form action="search.php">
+	<input type="search" id="mysrarch" name="search">
+	<button>Search</button>
 </body>
 </html>
