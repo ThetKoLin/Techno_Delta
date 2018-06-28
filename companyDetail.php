69663bc79bd01dc7843a059a719648c031ce3483
thetkolin <thetkolin2981834@gmail.com>
2018-06-28 08:43:37 +0630

    This code is to know company

---
+++ companyDetail.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Companies List</h1>
+<?php
+include("confs/config.php");
+$result = mysqli_query($conn, "SELECT * FROM companies WHERE CompanyName=$name");
+?>
+<?php while($row = mysqli_fetch_assoc($result)): ?>
+<?php	
+$name=$row["CompanyName"];
+$type=$row["CompanyType"];
+$address=$row["Address"];
+$ph=$row["PhoneNo"];
+echo "CompanyName-$name,CompanyType-$type,Address-$address,Phone-$ph";
+?>
+<br>
+<?php endwhile; ?>
+<a href="index.php" class="new">Home</a>
+</body>
+</html>
